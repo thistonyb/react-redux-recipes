79862526687baa94e789dcd245c274dd54d0be3e
TonyB <this.tonybaker@gmail.com>
2020-04-14 15:24:43 -0700

    Added index controller for Recipes

--- server/app/controllers/recipes_controller.rb
+++ server/app/controllers/recipes_controller.rb
@@ -1,2 +1,10 @@
 class RecipesController < ApplicationController
+    recipeKeys = [:id, :name, :source, :preptime, :waittime, :cooktime, :servings, :comments, :calories, :fat, :satfat, :carbs, :fiber, :sugar, :protein, :instructions, :ingredients, :tags]
+
+    def index
+        recipes = Recipe.all
+        render json: meals, only: recipeKeys    
+    end
+
+    
 end
