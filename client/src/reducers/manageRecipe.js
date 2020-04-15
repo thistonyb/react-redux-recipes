48a3e76dbd1160a3876d3dae25250fb3843321df
TonyB <this.tonybaker@gmail.com>
2020-04-14 17:11:08 -0700

    Wrote reducer - load recipes, add/delete review cases

--- client/src/reducers/manageRecipe.js
+++ client/src/reducers/manageRecipe.js
@@ -1,1 +1,33 @@
-import uuid from "uuid";
+const manageRecipe = (
+  state = { recipes: [], loading: false, reviews: [] },
+  action
+) => {
+  switch (action.type) {
+    case "LOADING_RECIPES":
+      return {
+        ...state,
+        recipes: [...state.recipes],
+        loading: true,
+      };
+    //Not sure what recipe id will be named recipeId/recipe_id
+    case "ADD_REVIEW":
+      const review = {
+        id: action.review.id,
+        comment: action.review.comment,
+        recipeId: action.review.recipeId,
+      };
+      return { ...state, reviews: [...state.reviews, review] };
+
+    case "DELETE_REVIEW":
+      return {
+        ...state,
+        reviews: state.reviews.filter(
+          (review) => review.id !== action.reviewId
+        ),
+      };
+
+    default:
+      return state;
+  }
+};
+export default manageRecipe;
