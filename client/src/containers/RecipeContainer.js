e9ec5677da4f40d2a3ee923b0474b0dbee16a26c
TonyB <this.tonybaker@gmail.com>
2020-04-15 15:03:48 -0700

    Made RecipeContainer class that renders Recipes.
    Mapped state and dispatch to props

--- client/src/containers/RecipeContainer.js
+++ client/src/containers/RecipeContainer.js
@@ -1,1 +1,28 @@
 import React, { Component } from "react";
+import Recipes from "../components/recipes/Recipes";
+import { connect } from "react-redux";
+import { fetchRecipes } from "./actions/RecipeActions";
+
+class RecipeContainer extends Component {
+  render() {
+    return (
+      <div>
+        <Recipes recipes={this.props.recipes} />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    recipes: state.recipes,
+    loading: state.loading,
+  };
+};
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchRecipes: () => dispatch(fetchRecipes()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(RecipeContainer);
