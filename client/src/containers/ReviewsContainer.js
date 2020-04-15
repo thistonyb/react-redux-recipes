540bf09821b3823e63f11a69aa2670455b55db9a
TonyB <this.tonybaker@gmail.com>
2020-04-15 15:30:29 -0700

    Made ReviewsContainer class, imported Compoent, connect and actions

---
+++ client/src/containers/ReviewsContainer.js
@@ -0,0 +1,11 @@
+import React, { Componet } from "react";
+import { connect } from "react-redux";
+import { addReview, deleteReview } from "../actions/ReviewActions";
+
+class ReviewsContainer extends Component {
+    render() {
+        return (
+            
+        )
+    }
+}
