6a99cc6e7c8b2d3af8bf80d6a588ad8a49c9e276
TonyB <this.tonybaker@gmail.com>
2020-04-16 16:23:08 -0700

    Made a route for backend to get recipes by tag

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :reviews
   resources :recipes
+
+  get '/recipes_by_tag/:tag', to: 'recipes#tag'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
