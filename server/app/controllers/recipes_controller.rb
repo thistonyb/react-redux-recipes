8c42ba51036f0cb6e6021503ea7685aaa1be9050
TonyB <this.tonybaker@gmail.com>
2020-04-17 13:54:22 -0700

    Fixed syntax error

--- server/app/controllers/recipes_controller.rb
+++ server/app/controllers/recipes_controller.rb
@@ -1,6 +1,4 @@
 class RecipesController < ApplicationController
-    recipeKeys = [:id, :name, :source, :preptime, :waittime, :cooktime, :servings, :comments, :calories, :fat, :satfat, :carbs, :fiber, :sugar, :protein, :instructions, :ingredients, :tags]
-
     def index
         recipes = Recipe.all
         render json: recipes, only: recipeKeys    
@@ -11,7 +9,7 @@ class RecipesController < ApplicationController
         filteredRecipes = recipes.select do |recipe|
             recipe[:tags].include? params[:tag]
         end
-        render json: filteredRecipes, only: recipeKeys  
+        render json: filteredRecipes, only: [:id, :name, :source, :preptime, :waittime, :cooktime, :servings, :comments, :calories, :fat, :satfat, :carbs, :fiber, :sugar, :protein, :instructions, :ingredients, :tags]  
     end
 
     
