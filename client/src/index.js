8cdf4193d98e5882bddf357e4f8033c74cd08322
TonyB <this.tonybaker@gmail.com>
2020-04-17 13:54:54 -0700

    Fixed file accessing error

--- client/src/index.js
+++ client/src/index.js
@@ -4,18 +4,18 @@ import "./index.css";
 import App from "./App";
 import manageRecipe from "./reducers/manageRecipe";
 import { Provider } from "react-redux";
-import { createStore } from redux;
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 //import * as serviceWorker from './serviceWorker';
 
-const store = createStore(manageRecipe)
+const store = createStore(manageRecipe, applyMiddleware(thunk));
 //Use Provider to provide a store with manageRecipe passed in
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-document.getElementById('root')
-)
-
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
