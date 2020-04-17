1bc8d0b70d093d74b8bb24601393a0ef12d40ad8
TonyB <this.tonybaker@gmail.com>
2020-04-17 13:57:14 -0700

    Put backend server on a different port than front

--- client/src/actions/RecipeActions.js
+++ client/src/actions/RecipeActions.js
@@ -1,7 +1,7 @@
 export const fetchRecipes = (tag) => {
   return (dispatch) => {
     dispatch({ type: "LOADING_RECIPES" });
-    fetch(`http://localhost:3000/recipes_by_tag/${tag}`)
+    fetch(`http://localhost:3001/recipes_by_tag/${tag}`)
       .then((response) => {
         return response.json();
       })
