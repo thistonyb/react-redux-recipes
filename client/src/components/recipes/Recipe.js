132cb14f58f213f3551a41de586dabaedb5b57d3
TonyB <this.tonybaker@gmail.com>
2020-04-18 15:00:07 -0700

    Wrote Recipe, imported NavLink

--- client/src/components/recipes/Recipe.js
+++ client/src/components/recipes/Recipe.js
@@ -1,9 +1,47 @@
 import React from "react";
-import ReviewsContainer from "../../containers/ReviewsContainer";
+import { NavLink } from "react-router-dom";
 
 const Recipe = (props) => {
-  return null;
-  // <li>{props.recipe.name}</li>
-  // <ReviewsContainer recipe={props.recipe} />
+  const { recipe } = props;
+  const ingredients = recipe.ingredients
+    .split(",")
+    .map((ingredient) => <li>{ingredient}</li>);
+  return (
+    <div className="recipe">
+      <h1>{recipe.name}</h1>
+      <NavLink to={`/recipes/${recipe.id}/reviews`}>Reviews</NavLink>
+      <br />
+      <br />
+      <span>{`Prep Time: ${recipe.preptime / 60} minutes`}</span>
+      <br />
+      <span>{`Wait Time: ${recipe.waittime / 60} minutes`}</span>
+      <br />
+      <span>{`Cook Time: ${recipe.cooktime / 60} minutes`}</span>
+      <br />
+      <span>{`Servings: ${recipe.servings}`}</span>
+      <br />
+      <span>{`Comments: ${recipe.comments}`}</span>
+      <br />
+      <span>{`Calories: ${recipe.calories}`}</span>
+      <br />
+      <span>{`Fat: ${recipe.fat} g`}</span>
+      <br />
+      <span>{`Sat Fat: ${recipe.satfat} g`}</span>
+      <br />
+      <span>{`Carbs: ${recipe.carbs} g`}</span>
+      <br />
+      <span>{`Fiber: ${recipe.fiber} g`}</span>
+      <br />
+      <span>{`Sugar: ${recipe.sugar} g`}</span>
+      <br />
+      <span>{`Protein: ${recipe.protein} g`}</span>
+      <h2>Instructions:</h2>
+      <p>{recipe.instructions}</p>
+      <h2>Ingredients:</h2>
+      <ul>{ingredients}</ul>
+      <h3>Tags:</h3>
+      <p>{recipe.tags.split(",").join(", ")}</p>
+    </div>
+  );
 };
 export default Recipe;
