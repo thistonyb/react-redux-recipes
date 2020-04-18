90f02f819c7146669cee75d9fbf77d718a39f5af
TonyB <this.tonybaker@gmail.com>
2020-04-18 15:00:49 -0700

    Fixed imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import RecipesContainer from "./containers/RecipesContainer";
 import Home from "./components/Home";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import RecipesRouter from "./components/recipes/RecipesRouter";
