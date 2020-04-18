267d7cc1331030c761f2c9b87939f598533e5163
TonyB <this.tonybaker@gmail.com>
2020-04-18 14:59:33 -0700

    Fixed Route and imports

--- client/src/components/recipes/RecipesRouter.js
+++ client/src/components/recipes/RecipesRouter.js
@@ -1,16 +1,12 @@
 import React from "react";
-import Recipe from "./Recipe";
 import { Route } from "react-router-dom";
-import RecipesContainer from "../../containers/RecipesContainer";
+import RecipesSearchContainer from "../../containers/RecipesSearchContainer";
+import RecipeContainer from "../../containers/RecipeContainer";
 
 const RecipesRouter = ({ match }) => (
   <div>
-    <Route exact path={match.url} component={RecipesContainer} />
-    {/* Write container for this Route */}
-    <Route
-      path={`${match.url}/:recipeId`}
-      render={(routerProps) => <Recipe {...routerProps} />}
-    />
+    <Route exact path={match.url} component={RecipesSearchContainer} />
+    <Route path={`${match.url}/:recipeId`} component={RecipeContainer} />
   </div>
 );
 
