ee1fc256b8b47d40441b1829242f81f33d2d7c05
TonyB <this.tonybaker@gmail.com>
2020-04-17 18:45:53 -0700

    Added Router to App component

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,19 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import RecipesContainer from "./containers/RecipesContainer";
+import Home from "./components/Home";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import RecipesRouter from "./components/recipes/RecipesRouter";
 
 export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <RecipesContainer />
-      </div>
+      <Router>
+        <div className="App">
+          <Route exact path="/" component={Home} />
+          <Route path="/recipes" component={RecipesRouter} />
+        </div>
+      </Router>
     );
   }
 }
