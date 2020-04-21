f8c2740edac7f3c23750b3b9db2c9a8ce2565c0f
TonyB <this.tonybaker@gmail.com>
2020-04-20 17:25:21 -0700

    Wrote Review  actions

--- client/src/actions/ReviewActions.js
+++ client/src/actions/ReviewActions.js
@@ -1,13 +1,40 @@
-export const addReview = (review) => {
-  return {
-    type: "ADD_REVIEW",
-    review,
+export const addReview = (comment, recipeId) => {
+  return (dispatch) => {
+    let data = { comment: comment, recipe_id: recipeId };
+    let configObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+    fetch("http://localhost:3001/reviews", configObj)
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseJSON) => {
+        dispatch({ type: "ADD_REVIEW", review: responseJSON });
+      });
   };
 };
 
 export const deleteReview = (reviewId) => {
-  return {
-    type: "DELETE_REVIEW",
-    reviewId,
+  return (dispatch) => {
+    let configObj = {
+      method: "DELETE",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    };
+    fetch(`http://localhost:3001/reviews/${reviewId}`, configObj)
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseJSON) => {
+        dispatch({ type: "DELETE_REVIEW", reviewId });
+      });
   };
 };
