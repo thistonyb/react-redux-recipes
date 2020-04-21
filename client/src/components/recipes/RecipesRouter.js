9dbeaa7e86534b887455ec5af8cf824ee147c51e
TonyB <this.tonybaker@gmail.com>
2020-04-20 17:30:49 -0700

    Added Route for reviews

--- client/src/components/recipes/RecipesRouter.js
+++ client/src/components/recipes/RecipesRouter.js
@@ -2,11 +2,16 @@ import React from "react";
 import { Route } from "react-router-dom";
 import RecipesSearchContainer from "../../containers/RecipesSearchContainer";
 import RecipeContainer from "../../containers/RecipeContainer";
+import ReviewsContainer from "../../containers/ReviewsContainer";
 
 const RecipesRouter = ({ match }) => (
   <div>
     <Route exact path={match.url} component={RecipesSearchContainer} />
-    <Route path={`${match.url}/:recipeId`} component={RecipeContainer} />
+    <Route exact path={`${match.url}/:recipeId`} component={RecipeContainer} />
+    <Route
+      path={`${match.url}/:recipeId/reviews`}
+      component={ReviewsContainer}
+    />
   </div>
 );
 
