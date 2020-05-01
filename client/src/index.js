57baee390e1d119a838f00a3e1ec5cb76c53e3ca
TonyB <this.tonybaker@gmail.com>
2020-05-01 14:31:47 -0700

    Added redux devtools extension

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,15 @@ import "./index.css";
 import App from "./App";
 import manageRecipe from "./reducers/manageRecipe";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 //import * as serviceWorker from './serviceWorker';
-
-const store = createStore(manageRecipe, applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  manageRecipe,
+  composeEnhancers(applyMiddleware(thunk))
+);
+//const store = createStore(manageRecipe, applyMiddleware(thunk));
 /**
  * Use Provider to provide a store with manageRecipe reducer and
  * thunk middleware (to incorporate asychronous code in our Redux
