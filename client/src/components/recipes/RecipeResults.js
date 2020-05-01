76dc9fba261c3a273767f3f206f3e07ba0e74de9
TonyB <this.tonybaker@gmail.com>
2020-05-01 14:27:48 -0700

    Removed commented out code

--- client/src/components/recipes/RecipeResults.js
+++ client/src/components/recipes/RecipeResults.js
@@ -1,23 +1,5 @@
 import React, { Component } from "react";
 import RecipeLink from "./RecipeLink";
-//rewrite this..should be a functional
-
-// class RecipeResults extends Component {
-//   render() {
-//     const recipes = this.props.recipes.map((recipe) => {
-//       return <RecipeLink key={recipe.id} recipe={recipe} />;
-//     });
-//     if (this.props.loading) {
-//       return <div>Loading Recipes</div>;
-//     }
-//     return (
-//       <div>
-//         <ul style={{ padding: 0 }}>{recipes}</ul>
-//       </div>
-//     );
-//   }
-// }
-
 /**
  * This functional component renders a RecipeLink for each of the
  * results of the recipes fetch.
