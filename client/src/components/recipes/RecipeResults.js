9bf4ce42eb17cd6e1fb3a35f4be1e4517271b830
TonyB <this.tonybaker@gmail.com>
2020-05-01 14:24:58 -0700

    Commented out code

--- client/src/components/recipes/RecipeResults.js
+++ client/src/components/recipes/RecipeResults.js
@@ -1,5 +1,23 @@
-import React from "react";
+import React, { Component } from "react";
 import RecipeLink from "./RecipeLink";
+//rewrite this..should be a functional
+
+// class RecipeResults extends Component {
+//   render() {
+//     const recipes = this.props.recipes.map((recipe) => {
+//       return <RecipeLink key={recipe.id} recipe={recipe} />;
+//     });
+//     if (this.props.loading) {
+//       return <div>Loading Recipes</div>;
+//     }
+//     return (
+//       <div>
+//         <ul style={{ padding: 0 }}>{recipes}</ul>
+//       </div>
+//     );
+//   }
+// }
+
 /**
  * This functional component renders a RecipeLink for each of the
  * results of the recipes fetch.
@@ -7,11 +25,14 @@ import RecipeLink from "./RecipeLink";
  * @param {*} props Props passed in from RecipesSearchContainer
  */
 const RecipeResults = (props) => {
+  console.log(props);
   const recipes = props.recipes.map((recipe) => (
     <RecipeLink key={recipe.id} recipe={recipe} />
   ));
+  if (props.loading) {
+    return <div>Loading Recipes</div>;
+  }
 
   return <ul style={{ padding: 0 }}>{recipes}</ul>;
 };
-
 export default RecipeResults;
