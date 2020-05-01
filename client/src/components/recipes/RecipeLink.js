1c4d03987be7d28921ea57da3b29b430259ac3d9
TonyB <this.tonybaker@gmail.com>
2020-05-01 14:26:43 -0700

    Made it into a class so it had local state for likes

--- client/src/components/recipes/RecipeLink.js
+++ client/src/components/recipes/RecipeLink.js
@@ -1,23 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 import "./RecipeLink.css";
 /**
- * Functional component that renders a NavLink for each recipe as well
+ * Class component that renders a NavLink for each recipe as well
  * as the tags that the recipe is associated with.
- * It handles removing a tag called "FUF"(not sure what it means).
+ * It handles removing a tag called "FUF"(was in the dataset, not sure what it means).
  * @param {*} props Props passed in from RecipeResults
  */
-const RecipeLink = (props) => (
-  <li className="recipeListItem">
-    <NavLink className="recipeLink" to={`/recipes/${props.recipe.id}`}>
-      {props.recipe.name}
-    </NavLink>
-    <p className="recipeTags">
-      {props.recipe.tags
-        .split(",")
-        .filter((tag) => tag !== "FUF")
-        .join(", ")}
-    </p>
-  </li>
-);
+class RecipeLink extends Component {
+  state = { likes: 0 };
+
+  handleOnClick = () => {
+    this.setState({
+      likes: this.state.likes + 1,
+    });
+  };
+
+  render() {
+    const { props } = this;
+    return (
+      <li className="recipeListItem">
+        <NavLink className="recipeLink" to={`/recipes/${props.recipe.id}`}>
+          {props.recipe.name}
+        </NavLink>
+        <p className="recipeTags">
+          {props.recipe.tags
+            .split(",")
+            .filter((tag) => tag !== "FUF")
+            .join(", ")}
+        </p>
+        <span>Likes: {this.state.likes}</span>
+        <button onClick={this.handleOnClick}>Like</button>
+      </li>
+    );
+  }
+}
+
 export default RecipeLink;
